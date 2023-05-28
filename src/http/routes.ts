cff279b1ff63e92ec51c85c394e0251544335bb3
Felipe <flpc.dev@gmail.com>
2023-05-28 03:15:23 -0300

    refactor: move endpoint to routes and his callback to controllers folder

---
+++ src/http/routes.ts
@@ -0,0 +1,6 @@
+import { register } from './controllers/register'
+import { FastifyInstance } from 'fastify'
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+}
