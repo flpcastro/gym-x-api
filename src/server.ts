ac1bbd02251268080d18973fa7bbe9658d46980b
Felipe <flpc.dev@gmail.com>
2023-05-28 01:25:00 -0300

    feat: create and treatment dot env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
-  console.log('🚀 HTTP server running on port 3333')
+  console.log(`🚀 HTTP server running on port ${env.PORT}`)
 })
