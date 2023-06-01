c627f37807d03ba0c283e4deef42051e4784aa59
Felipe <flpc.dev@gmail.com>
2023-06-01 15:44:28 -0300

    feat: create check in use case and first test

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,22 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+
+let inMemoryCheckInsRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('Check-in Use Case', () => {
+  beforeEach(() => {
+    inMemoryCheckInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(inMemoryCheckInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
