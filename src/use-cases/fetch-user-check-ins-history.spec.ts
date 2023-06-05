538f8c58dd8c572684839a72fb4fe5a29351fc30
Felipe <flpc.dev@gmail.com>
2023-06-05 02:58:08 -0300

    feat: create find many check ins by user id

---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,57 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { FetchUserCheckInsHistoryUseCase } from './fetch-user-check-ins-history'
+
+let inMemoryCheckInsRepository: InMemoryCheckInsRepository
+
+let sut: FetchUserCheckInsHistoryUseCase
+
+describe('Fetch User Check-in History Use Case', () => {
+  beforeEach(async () => {
+    inMemoryCheckInsRepository = new InMemoryCheckInsRepository()
+    sut = new FetchUserCheckInsHistoryUseCase(inMemoryCheckInsRepository)
+  })
+
+  it('should be able to fetch check-in history', async () => {
+    await inMemoryCheckInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await inMemoryCheckInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 1,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+
+  it('should be able to fetch paginated check-in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryCheckInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01',
+      })
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 2,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
+    ])
+  })
+})
