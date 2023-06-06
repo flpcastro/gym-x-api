eff5f4bc5293b493260b2e90895009259e1ceaef
Felipe <flpc.dev@gmail.com>
2023-06-05 22:41:57 -0300

    feat: configurate fastify jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', profile)
 }
