c7e50e3236d8c44291eb8aca452f2ea2d686b801
Felipe <flpc.dev@gmail.com>
2023-06-07 18:23:25 -0300

    refactor: move user controllers to folder users

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,10 @@
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
-import { register } from './controllers/register'
 import { FastifyInstance } from 'fastify'
-import { verifyJWT } from './middlewares/verify-jwt'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { register } from './register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
