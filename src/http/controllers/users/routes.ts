5ae117a1dea2b497fc605068f31a4b13986ddcca
Felipe <flpc.dev@gmail.com>
2023-06-07 18:32:03 -0300

    refactor: users routes name changed

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -6,7 +6,7 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
