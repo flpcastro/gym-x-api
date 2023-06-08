62b7e91161ebc24e244365b5613949b253616f6c
Felipe <flpc.dev@gmail.com>
2023-06-08 18:17:38 -0300

    feat: config refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import fastify from 'fastify'
+
 import { ZodError } from 'zod'
 import { env } from './env'
+
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
@@ -10,7 +14,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
