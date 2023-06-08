8ca37b4a0455a30b80eb12c6124032609694e0dc
Felipe <flpc.dev@gmail.com>
2023-06-08 04:21:42 -0300

    fix: request method

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -8,7 +8,7 @@ export async function search(request: FastifyRequest, reply: FastifyReply) {
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { query, page } = searchGymQuerySchema.parse(request.body)
+  const { query, page } = searchGymQuerySchema.parse(request.query)
 
   const searchGymUseCase = makeSearchGymsUseCase()
 
